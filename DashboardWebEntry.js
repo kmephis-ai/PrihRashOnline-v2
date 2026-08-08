c7477a5e5d29c7c1311bfbf86932809cf73f7ec1
kmephis-ai <kmephis@gmail.com>
2026-08-08 10:22:11 +0300

    fix: restore dashboard entry after template-scriptlet repair

--- DashboardWebEntry.js
+++ DashboardWebEntry.js
@@ -1,1 +1,9 @@
-function doGet(){return HtmlService.createHtmlOutput('PRIHRASH_RUNTIME_OK');}
+function doGet(e) {
+  var params = e && e.parameter ? e.parameter : {};
+  var template = HtmlService.createTemplateFromFile('DashboardWebApp');
+  var data = prhGetWebDashboardData(params.year, params.month, params.view);
+  var json = JSON.stringify(data);
+  var escapedLessThan = String.fromCharCode(92) + 'u003c';
+  template.initialData = json.split('<').join(escapedLessThan);
+  return template.evaluate();
+}
